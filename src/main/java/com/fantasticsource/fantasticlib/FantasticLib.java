283df5d19207ea71caff36a7e84a9becd5a321cc
Kyle Koder <silvertallon@gmail.com>
2019-02-22 13:49:16 -0800

    Initial commit

---
+++ src/main/java/com/fantasticsource/fantasticlib/FantasticLib.java
@@ -0,0 +1,22 @@
+package com.fantasticsource.fantasticlib;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = FantasticLib.MODID, name = FantasticLib.NAME, version = FantasticLib.VERSION)
+public class FantasticLib
+{
+    public static final String MODID = "fantasticlib";
+    public static final String NAME = "Fantastic Lib";
+    public static final String VERSION = "1.12.2.001";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+}
