62e0b5a4e9aef314bafaac638211988838d98eb1
Kyle Koder <silvertallon@gmail.com>
2019-03-09 08:59:10 -0800

    Improved potion parsing methods

--- src/main/java/com/fantasticsource/mctools/potions/Potions.java
+++ src/main/java/com/fantasticsource/mctools/potions/Potions.java
@@ -11,7 +11,7 @@ import java.util.regex.Pattern;
 public class Potions
 {
     /**
-     * Syntax is registrlyname.duration.level & registrlyname.duration.level & registrlyname.duration.level...
+     * Syntax is registryname.duration.level & registrlyname.duration.level & registrlyname.duration.level...
      */
     public static ArrayList<PotionEffect> parsePotions(String potionList)
     {
@@ -21,49 +21,53 @@ public class Potions
     }
 
     /**
-     * Syntax for each is registrlyname.duration.level
+     * Syntax for each is registryname.duration.level
      */
     public static ArrayList<PotionEffect> parsePotions(String[] potionList)
+    {
+        ArrayList<PotionEffect> result = new ArrayList<>();
+
+        PotionEffect potion;
+        for (String string : potionList)
+        {
+            potion = parsePotion(string);
+            if (potion != null) result.add(potion);
+        }
+
+        return result;
+    }
+
+    /**
+     * Syntax is registryname.duration.level
+     */
+    public static PotionEffect parsePotion(String potionString)
     {
         String[] tokens;
         int duration, amplifier;
         Potion potion;
-        ArrayList<PotionEffect> result = new ArrayList<>();
 
-        for (String string : potionList)
+        potionString = potionString.trim();
+        if (potionString.equals("")) return null;
+
+        tokens = potionString.split(Pattern.quote("."));
+        if (tokens.length <= 0) return null;
+        if (tokens.length > 3)
         {
-            string = string.trim();
-            if (!string.equals(""))
-            {
-                tokens = string.split(Pattern.quote("."));
-                if (tokens.length > 3)
-                {
-                    System.err.println("Too many arguments for potion effect; should be max of 3");
-                    continue;
-                }
-
-                duration = 0;
-                amplifier = 0;
-
-                if (tokens.length > 0)
-                {
-                    potion = ForgeRegistries.POTIONS.getValue(new ResourceLocation(tokens[0].trim()));
-
-                    if (potion == null)
-                    {
-                        System.err.println("ResourceLocation for potion \"" + string + "\" not found!");
-                        continue;
-                    }
-
-                    if (tokens.length > 1) duration = Integer.parseInt(tokens[1].trim());
-                    if (tokens.length > 2) amplifier = Integer.parseInt(tokens[2].trim());
-                    if (amplifier > 0) amplifier--; //Makes it so ppl can just type 2 for stength 2 instead of typing 1
-
-                    result.add(new PotionEffect(potion, duration, amplifier, false, true));
-                }
-            }
+            System.err.println("Too many arguments for potion effect; should be max of 3");
+            return null;
         }
 
-        return result;
+        potion = ForgeRegistries.POTIONS.getValue(new ResourceLocation(tokens[0].trim()));
+        if (potion == null)
+        {
+            System.err.println("ResourceLocation for potion \"" + potionString + "\" not found!");
+            return null;
+        }
+
+        duration = tokens.length > 1 ? Integer.parseInt(tokens[1].trim()) : 0;
+        amplifier = tokens.length > 2 ? Integer.parseInt(tokens[2].trim()) : 0;
+        if (amplifier > 0) amplifier--; //Makes it so ppl can just type 2 for stength 2 instead of typing 1
+
+        return new PotionEffect(potion, duration, amplifier, false, true);
     }
 }
