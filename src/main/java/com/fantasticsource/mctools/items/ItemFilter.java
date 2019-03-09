a0e54fac57f50ed394c92b7dfe108fcc5961e682
Kyle Koder <silvertallon@gmail.com>
2019-03-09 14:04:53 -0800

    Think the item filter should be working, but haven't tested yet

--- src/main/java/com/fantasticsource/mctools/items/ItemFilter.java
+++ src/main/java/com/fantasticsource/mctools/items/ItemFilter.java
@@ -1,7 +1,6 @@
 package com.fantasticsource.mctools.items;
 
 import net.minecraft.block.Block;
-import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
@@ -16,55 +15,45 @@ import java.util.regex.Pattern;
 public class ItemFilter
 {
     public ItemStack itemStack = null;
-    private LinkedHashMap<String, String> tags = new LinkedHashMap<>();
+    private LinkedHashMap<String, String> tagsRequired = new LinkedHashMap<>();
+    private LinkedHashMap<String, String> tagsDisallowed = new LinkedHashMap<>();
 
 
     /**
      * Syntax is domain:item:meta > nbtkey1 = nbtvalue1 & nbtkey2 = nbtvalue2
-     * All of these are optional, except that each nbt value requires a key
+     * All of these are optional except item
      * <p>
-     * Domain and meta are optional as well
-     * If you only want to specify a domain, add a : after it, eg....
-     * minecraft:
+     * Each nbt value requires a key, but not necessarily a value (if no value is specified, it just checks if the key exists)
+     * Each NBT entry can be negated by starting it with a !
+     * eg...
+     * !generic.attackDamage
+     * !generic.attackDamage = 4
      * <p>
      * Examples...
      * diamond_sword
      * dye:0
-     * > generic.attackDamage = 10
-     * minecraft: > generic.attackDamage
+     * tetra:duplex_tool_modular > duplex/sickle_left_material & duplex/butt_right_material
      */
     public ItemFilter(String itemStackString)
     {
-        //Defaults
-        this(type);
-
-
-        String[] tokens = configEntry.split(Pattern.quote(","));
+        String[] registryAndNBT = itemStackString.trim().split(Pattern.quote(">"));
         String token;
 
-        if (tokens.length < 2)
+        if (registryAndNBT.length == 0)
         {
-            System.err.println("Not enough arguments for weapon entry: " + configEntry);
+            System.err.println("Not enough arguments for item filter: " + itemStackString);
             return;
         }
-        if (((type == TYPE_NORMAL || type == TYPE_STEALTH) && tokens.length > 6) || (type == TYPE_ASSASSINATION && tokens.length > 2))
-        {
-            System.err.println("Too many arguments for weapon entry: " + configEntry);
-            return;
-        }
-
-        String[] registryAndNBT = tokens[0].trim().split(Pattern.quote(">"));
         if (registryAndNBT.length > 2)
         {
-            System.err.println("Too many arguments for name/NBT in weapon entry: " + tokens[0]);
+            System.err.println("Too many arguments for item filter: " + itemStackString);
             return;
         }
 
 
         //Item and meta
         token = registryAndNBT[0].trim();
-        if (token.equals("")) itemStack = new ItemStack(Items.AIR);
-        else
+        if (!token.equals(""))
         {
             ResourceLocation resourceLocation;
             int meta = 0;
@@ -106,14 +95,12 @@ public class ItemFilter
                 Block block = ForgeRegistries.BLOCKS.containsKey(resourceLocation) ? ForgeRegistries.BLOCKS.getValue(resourceLocation) : null;
                 if (block != null) itemStack = new ItemStack(block, 1, Integer.parseInt(innerTokens[2]));
             }
-        }
 
-        if (itemStack == null)
-        {
-            if (type == TYPE_NORMAL && !AttackDefaults.normalAttackDefaults.contains(configEntry)) System.err.println("Item for normal attack weapon entry not found: " + token);
-            if (type == TYPE_STEALTH && !AttackDefaults.stealthAttackDefaults.contains(configEntry)) System.err.println("Item for stealth attack weapon entry not found: " + token);
-            if (type == TYPE_ASSASSINATION && !AttackDefaults.assassinationDefaults.contains(configEntry)) System.err.println("Item for assassination weapon entry not found: " + token);
-            return;
+            if (itemStack == null)
+            {
+                System.err.println("Item for item filter not found: " + token);
+                return;
+            }
         }
 
 
@@ -129,34 +116,59 @@ public class ItemFilter
                 String[] keyValue = tag.split(Pattern.quote("="));
                 if (keyValue.length > 2)
                 {
-                    System.err.println("Each NBT tag can only be set to one value!  Error in weapon entry: " + configEntry);
+                    System.err.println("Each NBT tag can only be set to one value!  Error in item filter: " + itemStackString);
                     return;
                 }
 
                 String key = keyValue[0].trim();
-                if (!key.equals("")) this.tags.put(key, keyValue.length == 2 ? keyValue[1].trim() : null);
+                if (!key.equals(""))
+                {
+                    if (key.charAt(0) == '!') tagsDisallowed.put(key, keyValue.length == 2 ? keyValue[1].trim() : null);
+                    else tagsRequired.put(key, keyValue.length == 2 ? keyValue[1].trim() : null);
+                }
             }
         }
     }
 
-    public boolean matches(ItemStack itemStack)
+
+    public boolean matches(ItemStack stack)
     {
+        //Domain, item, and meta
+        if (!(itemStack.getItem().equals(stack.getItem()) && (stack.isItemStackDamageable() || itemStack.getMetadata() == stack.getMetadata()))) return false;
 
-        if (!(this.itemStack.getItem().equals(itemStack.getItem()) && (itemStack.isItemStackDamageable() || this.itemStack.getMetadata() == itemStack.getMetadata()))) return false;
 
-        Set<Map.Entry<String, String>> entrySet = tags.entrySet();
-        if (entrySet.size() == 0) return true;
+        //Disallowed NBT
+        NBTTagCompound compound = stack.getTagCompound();
 
-        NBTTagCompound compound = itemStack.getTagCompound();
-        if (compound == null) return false;
-
-        for (Map.Entry<String, String> entry : entrySet)
+        if (compound != null)
         {
-            if (!compound.hasKey(entry.getKey()) || (entry.getValue() != null && !compound.getTag(entry.getKey()).toString().equals(entry.getValue())))
+            for (Map.Entry<String, String> entry : tagsDisallowed.entrySet())
             {
-                return false;
+                if (!(!compound.hasKey(entry.getKey()) || (entry.getValue() != null && !compound.getTag(entry.getKey()).toString().equals(entry.getValue()))))
+                {
+                    return false;
+                }
             }
         }
+
+
+        //Required NBT
+        Set<Map.Entry<String, String>> entrySet = tagsRequired.entrySet();
+        if (entrySet.size() > 0)
+        {
+            if (compound == null) return false;
+
+            for (Map.Entry<String, String> entry : entrySet)
+            {
+                if (!compound.hasKey(entry.getKey()) || (entry.getValue() != null && !compound.getTag(entry.getKey()).toString().equals(entry.getValue())))
+                {
+                    return false;
+                }
+            }
+        }
+
+
+        //Passed all filters
         return true;
     }
 }
