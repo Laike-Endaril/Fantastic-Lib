081d7a8c132c1f3b1ef9b9cddb8eb3341efa726a
Kyle Koder <silvertallon@gmail.com>
2019-03-11 00:37:22 -0700

    Added prototype attribute modifier parser

---
+++ src/main/java/com/fantasticsource/mctools/attributes/AttributeMods.java
@@ -0,0 +1,57 @@
+package com.fantasticsource.mctools.attributes;
+
+import net.minecraft.entity.ai.attributes.AttributeModifier;
+
+import java.util.ArrayList;
+
+public class AttributeMods
+{
+    /**
+     * Syntax is attribute = amount @ operation, attribute = amount @ operation, attribute = amount @ operation...
+     */
+    public static ArrayList<AttributeModifier> parseMods(String modList)
+    {
+        String[] modifiers = modList.split("&");
+        for (int i = 0; i < modifiers.length; i++) modifiers[i] = modifiers[i].trim();
+        return parseMods(modifiers);
+    }
+
+    /**
+     * Syntax for each is attribute = amount @ operation
+     */
+    public static ArrayList<AttributeModifier> parseMods(String[] modList)
+    {
+        ArrayList<AttributeModifier> result = new ArrayList<>();
+
+        AttributeModifier modifier;
+        for (String string : modList)
+        {
+            modifier = parseMod(string);
+            if (modifier != null) result.add(modifier);
+        }
+
+        return result;
+    }
+
+    /**
+     * Syntax is attribute = amount @ operation
+     */
+    public static AttributeModifier parseMod(String modString)
+    {
+        String[] tokens = modString.split("=");
+        if (tokens.length < 2 || tokens.length > 3)
+        {
+            System.err.println("Malformed attribute modifier string: " + modString);
+            return null;
+        }
+
+        String[] tokens2 = tokens[1].split("@");
+        if (tokens2.length > 2)
+        {
+            System.err.println("Malformed attribute modifier string: " + modString);
+            return null;
+        }
+
+        return new AttributeModifier(tokens[0].trim(), Double.parseDouble(tokens2[0]), tokens2.length > 1 ? Integer.parseInt(tokens2[1]) : 0);
+    }
+}
