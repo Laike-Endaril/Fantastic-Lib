941019e21860e00b617be0486db41f7a1921803e
Kyle Koder <silvertallon@gmail.com>
2019-03-11 00:27:13 -0700

    Potions that don't have a duration specified when parsed now have "infinite" duration (Integer.MAX_VALUE)
    Fixed comment typos

--- src/main/java/com/fantasticsource/mctools/potions/Potions.java
+++ src/main/java/com/fantasticsource/mctools/potions/Potions.java
@@ -11,7 +11,7 @@ import java.util.regex.Pattern;
 public class Potions
 {
     /**
-     * Syntax is registryname.duration.level & registrlyname.duration.level & registrlyname.duration.level...
+     * Syntax is registryname.duration.level & registryname.duration.level & registryname.duration.level...
      */
     public static ArrayList<PotionEffect> parsePotions(String potionList)
     {
@@ -50,7 +50,11 @@ public class Potions
         if (potionString.equals("")) return null;
 
         tokens = potionString.split(Pattern.quote("."));
-        if (tokens.length <= 0) return null;
+        if (tokens.length < 1)
+        {
+            System.err.println("Not enough arguments for potion effect; should be min of 1");
+            return null;
+        }
         if (tokens.length > 3)
         {
             System.err.println("Too many arguments for potion effect; should be max of 3");
@@ -64,7 +68,7 @@ public class Potions
             return null;
         }
 
-        duration = tokens.length > 1 ? Integer.parseInt(tokens[1].trim()) : 0;
+        duration = tokens.length > 1 ? Integer.parseInt(tokens[1].trim()) : Integer.MAX_VALUE;
         amplifier = tokens.length > 2 ? Integer.parseInt(tokens[2].trim()) : 0;
         if (amplifier > 0) amplifier--; //Makes it so ppl can just type 2 for stength 2 instead of typing 1
 
