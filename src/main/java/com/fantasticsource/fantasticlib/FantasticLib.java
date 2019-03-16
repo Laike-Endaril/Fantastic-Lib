f9300b936f416578d71bc5b4743aa1905b5a71f1
Kyle Koder <silvertallon@gmail.com>
2019-03-16 15:58:32 -0700

    1.12.2.007

--- src/main/java/com/fantasticsource/fantasticlib/FantasticLib.java
+++ src/main/java/com/fantasticsource/fantasticlib/FantasticLib.java
@@ -13,7 +13,7 @@ public class FantasticLib
 {
     public static final String MODID = "fantasticlib";
     public static final String NAME = "Fantastic Lib";
-    public static final String VERSION = "1.12.2.006a";
+    public static final String VERSION = "1.12.2.007";
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event)
