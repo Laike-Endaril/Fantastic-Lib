4834242c70157a2aabece7351707f83b7738cef9
Kyle Koder <silvertallon@gmail.com>
2019-03-18 15:00:27 -0700

    Revert "1.12.2.008"
    This reverts commit 00712baf25cade936d1b0634cc732b410fc7ff22.

--- src/main/java/com/fantasticsource/fantasticlib/FantasticLib.java
+++ src/main/java/com/fantasticsource/fantasticlib/FantasticLib.java
@@ -17,7 +17,7 @@ public class FantasticLib
 {
     public static final String MODID = "fantasticlib";
     public static final String NAME = "Fantastic Lib";
-    public static final String VERSION = "1.12.2.008";
+    public static final String VERSION = "1.12.2.007a";
 
     public FantasticLib()
     {
