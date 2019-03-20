72c9223ea338457e2085b42ff0bc317ace27ea3a
Kyle Koder <silvertallon@gmail.com>
2019-03-20 14:44:20 -0700

    Added lang key for config tooltip

--- src/main/java/com/fantasticsource/fantasticlib/config/FantasticConfig.java
+++ src/main/java/com/fantasticsource/fantasticlib/config/FantasticConfig.java
@@ -8,11 +8,6 @@ public class FantasticConfig
 {
     @Config.Name("Use Entity Render Fixer")
     @Config.LangKey(FantasticLib.MODID + ".config.entityRenderFixer")
-    @Config.Comment(
-            {
-                    "If enabled, runs some code to reset certain openGL settings after each entity renders",
-                    "Only use this if you need it to fix a graphical issue"
-            })
     @Config.RequiresMcRestart
     public static boolean entityRenderFixer = false;
 }
