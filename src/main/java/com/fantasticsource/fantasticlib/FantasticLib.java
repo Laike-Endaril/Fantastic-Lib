a1e11298ddbefb7b7c2ba86dabf9edfd93ef7fc6
Kyle Koder <silvertallon@gmail.com>
2019-06-26 19:26:35 -0700

    Indev build 1.12.2.019a (accepts all remote versions temporarily)

--- src/main/java/com/fantasticsource/fantasticlib/FantasticLib.java
+++ src/main/java/com/fantasticsource/fantasticlib/FantasticLib.java
@@ -12,12 +12,12 @@ import net.minecraftforge.fml.common.event.FMLServerAboutToStartEvent;
 import net.minecraftforge.fml.common.event.FMLServerStoppedEvent;
 import net.minecraftforge.fml.relauncher.Side;
 
-@Mod(modid = FantasticLib.MODID, name = FantasticLib.NAME, version = FantasticLib.VERSION)
+@Mod(modid = FantasticLib.MODID, name = FantasticLib.NAME, version = FantasticLib.VERSION, acceptableRemoteVersions = "*")
 public class FantasticLib
 {
     public static final String MODID = "fantasticlib";
     public static final String NAME = "Fantastic Lib";
-    public static final String VERSION = "1.12.2.019";
+    public static final String VERSION = "1.12.2.019a";
 
     public FantasticLib()
     {
