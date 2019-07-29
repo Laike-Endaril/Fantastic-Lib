97eb859c227c3c2c86b3c60e11f993af06765eca
Kyle Koder <silvertallon@gmail.com>
2019-07-28 23:26:01 -0700

    Text selection via keyboard is perfect

--- src/main/java/com/fantasticsource/mctools/gui/guielements/rect/GUITextInputRect.java
+++ src/main/java/com/fantasticsource/mctools/gui/guielements/rect/GUITextInputRect.java
@@ -3,6 +3,7 @@ package com.fantasticsource.mctools.gui.guielements.rect;
 import com.fantasticsource.mctools.gui.GUILeftClickEvent;
 import com.fantasticsource.mctools.gui.GUIScreen;
 import com.fantasticsource.mctools.gui.guielements.GUIElement;
+import com.fantasticsource.tools.Tools;
 import com.fantasticsource.tools.datastructures.Color;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraftforge.common.MinecraftForge;
@@ -13,16 +14,17 @@ import java.util.ArrayList;
 
 public class GUITextInputRect extends GUITextRect
 {
-    protected int cursorPosition;
-    protected Color cursorColor;
+    protected int cursorPosition, selectorPosition = -1;
+    protected Color cursorColor, highlightColor;
     protected long cursorTime;
 
-    public GUITextInputRect(GUIScreen screen, double x, double y, String text, Color color, Color hoverColor, Color activeColor, Color cursorColor)
+    public GUITextInputRect(GUIScreen screen, double x, double y, String text, Color color, Color hoverColor, Color activeColor, Color cursorColor, Color hightlightColor)
     {
         super(screen, x, y, text, color, hoverColor, activeColor);
 
         cursorPosition = text.length();
         this.cursorColor = cursorColor;
+        this.highlightColor = hightlightColor;
     }
 
     @Override
@@ -56,10 +58,22 @@ public class GUITextInputRect extends GUITextRect
         }
         else if (keyCode == Keyboard.KEY_LEFT)
         {
+            if (GUIScreen.isShiftKeyDown())
+            {
+                if (selectorPosition == -1 && cursorPosition > 0) selectorPosition = cursorPosition;
+            }
+            else selectorPosition = -1;
+
             if (cursorPosition > 0) cursorPosition--;
         }
         else if (keyCode == Keyboard.KEY_RIGHT)
         {
+            if (GUIScreen.isShiftKeyDown())
+            {
+                if (selectorPosition == -1 && cursorPosition < text.length()) selectorPosition = cursorPosition;
+            }
+            else selectorPosition = -1;
+
             if (cursorPosition < text.length()) cursorPosition++;
         }
 
@@ -96,16 +110,33 @@ public class GUITextInputRect extends GUITextRect
         Color c = active ? activeColor : isMouseWithin() ? hoverColor : color;
         fontRenderer.drawString(text, 0, 0, (c.color() >> 8) | c.a() << 24, false);
 
-        if (active && (System.currentTimeMillis() - cursorTime) % 1000 < 500)
+        if (active)
         {
-            float xx = fontRenderer.getStringWidth(text.substring(0, cursorPosition)) - 0.5f;
+            float cursorX = fontRenderer.getStringWidth(text.substring(0, cursorPosition)) - 0.5f;
+            float selectorX = selectorPosition == -1 ? cursorX : fontRenderer.getStringWidth(text.substring(0, selectorPosition)) - 0.5f;
 
-            GlStateManager.disableTexture2D();
-            GlStateManager.color(cursorColor.rf(), cursorColor.gf(), cursorColor.bf(), cursorColor.af());
-            GlStateManager.glBegin(GL11.GL_LINES);
-            GlStateManager.glVertex3f(xx, -0.5f, 0);
-            GlStateManager.glVertex3f(xx, fontRenderer.FONT_HEIGHT - 1, 0);
-            GlStateManager.glEnd();
+            if (cursorX != selectorX)
+            {
+                float min = Tools.min(cursorX, selectorX), max = Tools.max(cursorX, selectorX);
+                GlStateManager.disableTexture2D();
+                GlStateManager.color(highlightColor.rf(), highlightColor.gf(), highlightColor.bf(), highlightColor.af());
+                GlStateManager.glBegin(GL11.GL_QUADS);
+                GlStateManager.glVertex3f(min, -0.5f, 0);
+                GlStateManager.glVertex3f(min, fontRenderer.FONT_HEIGHT - 1, 0);
+                GlStateManager.glVertex3f(max, fontRenderer.FONT_HEIGHT - 1, 0);
+                GlStateManager.glVertex3f(max, -0.5f, 0);
+                GlStateManager.glEnd();
+            }
+
+            if ((System.currentTimeMillis() - cursorTime) % 1000 < 500)
+            {
+                GlStateManager.disableTexture2D();
+                GlStateManager.color(cursorColor.rf(), cursorColor.gf(), cursorColor.bf(), cursorColor.af());
+                GlStateManager.glBegin(GL11.GL_LINES);
+                GlStateManager.glVertex3f(cursorX, -0.5f, 0);
+                GlStateManager.glVertex3f(cursorX, fontRenderer.FONT_HEIGHT - 1, 0);
+                GlStateManager.glEnd();
+            }
         }
 
         GlStateManager.popMatrix();
