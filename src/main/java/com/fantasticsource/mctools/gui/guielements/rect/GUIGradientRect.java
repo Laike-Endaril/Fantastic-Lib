2a960c70a3a94b2ebc384fb332c238b338f54dfb
Kyle Koder <silvertallon@gmail.com>
2019-07-30 01:06:14 -0700

    GUIGradientRect now supports a active / hover colors, and has additional constructors

--- src/main/java/com/fantasticsource/mctools/gui/guielements/rect/GUIGradientRect.java
+++ src/main/java/com/fantasticsource/mctools/gui/guielements/rect/GUIGradientRect.java
@@ -8,15 +8,30 @@ import static org.lwjgl.opengl.GL11.GL_QUADS;
 
 public class GUIGradientRect extends GUIRectElement
 {
-    private Color topRight, topLeft, bottomLeft, bottomRight;
+    private Color[] colors, hoverColors, activeColors;
+
+    public GUIGradientRect(GUIScreen screen, double x, double y, double width, double height, Color color)
+    {
+        this(screen, x, y, width, height, color, color, color);
+    }
+
+    public GUIGradientRect(GUIScreen screen, double x, double y, double width, double height, Color color, Color hover, Color active)
+    {
+        this(screen, x, y, width, height, color, color, color, color, hover, hover, hover, hover, active, active, active, active);
+    }
 
     public GUIGradientRect(GUIScreen screen, double x, double y, double width, double height, Color topRight, Color topLeft, Color bottomLeft, Color bottomRight)
     {
+        this(screen, x, y, width, height, topRight, topLeft, bottomLeft, bottomRight, topRight, topLeft, bottomLeft, bottomRight, topRight, topLeft, bottomLeft, bottomRight);
+    }
+
+    public GUIGradientRect(GUIScreen screen, double x, double y, double width, double height, Color topRight, Color topLeft, Color bottomLeft, Color bottomRight, Color topRightHover, Color topLeftHover, Color bottomLeftHover, Color bottomRightHover, Color topRightActive, Color topLeftActive, Color bottomLeftActive, Color bottomRightActive)
+    {
         super(screen, x, y, width, height);
-        this.topRight = topRight;
-        this.topLeft = topLeft;
-        this.bottomLeft = bottomLeft;
-        this.bottomRight = bottomRight;
+
+        colors = new Color[]{topRight, topLeft, bottomLeft, bottomRight};
+        hoverColors = new Color[]{topRightHover, topLeftHover, bottomLeftHover, bottomRightHover};
+        activeColors = new Color[]{topRightActive, topLeftActive, bottomLeftActive, bottomRightActive};
     }
 
     @Override
@@ -30,14 +45,16 @@ public class GUIGradientRect extends GUIRectElement
         float x2 = (float) (x1 + width);
         float y2 = (float) (y1 + height);
 
+        Color[] colors = active ? activeColors : isMouseWithin() ? hoverColors : this.colors;
+
         GlStateManager.glBegin(GL_QUADS);
-        GlStateManager.color(topRight.rf(), topRight.gf(), topRight.bf(), topRight.af());
+        GlStateManager.color(colors[0].rf(), colors[0].gf(), colors[0].bf(), colors[0].af());
         GlStateManager.glVertex3f(x2, y1, 0);
-        GlStateManager.color(topLeft.rf(), topLeft.gf(), topLeft.bf(), topLeft.af());
+        GlStateManager.color(colors[1].rf(), colors[1].gf(), colors[1].bf(), colors[1].af());
         GlStateManager.glVertex3f(x1, y1, 0);
-        GlStateManager.color(bottomLeft.rf(), bottomLeft.gf(), bottomLeft.bf(), bottomLeft.af());
+        GlStateManager.color(colors[2].rf(), colors[2].gf(), colors[2].bf(), colors[2].af());
         GlStateManager.glVertex3f(x1, y2, 0);
-        GlStateManager.color(bottomRight.rf(), bottomRight.gf(), bottomRight.bf(), bottomRight.af());
+        GlStateManager.color(colors[3].rf(), colors[3].gf(), colors[3].bf(), colors[3].af());
         GlStateManager.glVertex3f(x2, y2, 0);
         GlStateManager.glEnd();
 
