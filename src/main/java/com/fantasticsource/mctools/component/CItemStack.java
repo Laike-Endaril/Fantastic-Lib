ef38561db61e3b6ffd4d8aa830002d56b2303c12
Kyle Koder <silvertallon@gmail.com>
2019-09-14 12:23:11 -0700

    CItemStack

---
+++ src/main/java/com/fantasticsource/mctools/component/CItemStack.java
@@ -0,0 +1,61 @@
+package com.fantasticsource.mctools.component;
+
+import com.fantasticsource.tools.component.CStringUTF8;
+import com.fantasticsource.tools.component.Component;
+import io.netty.buffer.ByteBuf;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.JsonToNBT;
+import net.minecraft.nbt.NBTException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class CItemStack extends Component
+{
+    public ItemStack stack = ItemStack.EMPTY;
+
+    @Override
+    public Component write(ByteBuf buf)
+    {
+        new CStringUTF8().set(stack.serializeNBT().toString()).write(buf);
+        return this;
+    }
+
+    @Override
+    public Component read(ByteBuf buf)
+    {
+        try
+        {
+            stack = new ItemStack(JsonToNBT.getTagFromJson(new CStringUTF8().read(buf).value));
+            return this;
+        }
+        catch (NBTException e)
+        {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @Override
+    public Component save(OutputStream stream) throws IOException
+    {
+        new CStringUTF8().set(stack.serializeNBT().toString()).save(stream);
+        return this;
+    }
+
+    @Override
+    public Component load(InputStream stream) throws IOException
+    {
+        try
+        {
+            stack = new ItemStack(JsonToNBT.getTagFromJson(new CStringUTF8().load(stream).value));
+            return this;
+        }
+        catch (NBTException e)
+        {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
