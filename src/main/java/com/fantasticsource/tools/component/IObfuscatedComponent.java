3436cf067644e916f5137f5d8635368d98d6ed18
Kyle Koder <silvertallon@gmail.com>
2019-09-15 15:54:22 -0700

    IObfuscatedComponent.readMarkedObf() / writeMarkedObf()

--- src/main/java/com/fantasticsource/tools/component/IObfuscatedComponent.java
+++ src/main/java/com/fantasticsource/tools/component/IObfuscatedComponent.java
@@ -4,6 +4,26 @@ import io.netty.buffer.ByteBuf;
 
 public interface IObfuscatedComponent
 {
+    public static <T extends Component & IObfuscatedComponent> T writeMarkedObf(ByteBuf buf, T component)
+    {
+        new CStringUTF8().set(component.getClass().getName()).write(buf);
+        component.writeObf(buf);
+        return component;
+    }
+
+    public static Component readMarkedObf(ByteBuf buf)
+    {
+        try
+        {
+            return ((Component & IObfuscatedComponent) Class.forName(new CStringUTF8().read(buf).value).newInstance()).readObf(buf);
+        }
+        catch (InstantiationException | IllegalAccessException | ClassNotFoundException e)
+        {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
     Component writeObf(ByteBuf buf);
 
     Component readObf(ByteBuf buf);
