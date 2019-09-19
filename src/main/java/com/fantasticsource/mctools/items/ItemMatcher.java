37f76d9b15f847bea0f5380587946d14ad41b485
Kyle Koder <silvertallon@gmail.com>
2019-09-18 22:35:48 -0700

    Basic itemstack matching method

---
+++ src/main/java/com/fantasticsource/mctools/items/ItemMatcher.java
@@ -0,0 +1,16 @@
+package com.fantasticsource.mctools.items;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+
+public class ItemMatcher
+{
+    public static boolean stacksMatch(ItemStack stack1, ItemStack stack2)
+    {
+        Item item1 = stack1.getItem();
+        if (!item1.equals(stack2.getItem())) return false;
+        if (!item1.isDamageable() && stack1.getMetadata() != stack2.getMetadata()) return false;
+        if (stack1.hasTagCompound() != stack2.hasTagCompound()) return false;
+        return !stack1.hasTagCompound() || stack1.getTagCompound().equals(stack2.getTagCompound());
+    }
+}
