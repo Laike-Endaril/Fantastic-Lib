006048d58867a70b0632762e9b32803e3a62b9ad
Kyle Koder <silvertallon@gmail.com>
2019-10-04 17:33:59 -0700

    All current tests passed.  Now to test with mods

--- src/main/java/com/fantasticsource/mctools/gui/screen/TestGUI.java
+++ src/main/java/com/fantasticsource/mctools/gui/screen/TestGUI.java
@@ -2,11 +2,6 @@ package com.fantasticsource.mctools.gui.screen;
 
 import com.fantasticsource.mctools.gui.GUIScreen;
 import com.fantasticsource.mctools.gui.element.other.GUIGradient;
-import com.fantasticsource.mctools.gui.element.other.GUIGradientBorder;
-import com.fantasticsource.mctools.gui.element.text.GUIText;
-import com.fantasticsource.mctools.gui.element.text.GUITextInput;
-import com.fantasticsource.mctools.gui.element.text.filter.FilterNotEmpty;
-import com.fantasticsource.mctools.gui.element.view.GUIView;
 import com.fantasticsource.tools.datastructures.Color;
 import net.minecraft.client.Minecraft;
 import net.minecraft.util.EnumHand;
@@ -31,34 +26,6 @@ public class TestGUI extends GUIScreen
         {
             default:
                 test = 5;
-
-            case 5:
-            {
-                GUIView view = new GUIView(this, 0.25, 0.25, 0.5, 0.5);
-                root.add(view);
-                view.add(new GUITextInput(this, "Test", FilterNotEmpty.INSTANCE));
-                break;
-            }
-
-            case 4:
-                root.add(new GUITextInput(this, "Test", FilterNotEmpty.INSTANCE));
-                break;
-
-            case 3:
-            {
-                GUIView view = new GUIView(this, 0.25, 0.25, 0.5, 0.5);
-                root.add(view);
-                view.add(new GUIGradient(this, -1, -1, 3, 3, Color.WHITE));
-                break;
-            }
-
-            case 2:
-                root.add(new GUIText(this, "Test"));
-                break;
-
-            case 1:
-                root.add(new GUIGradientBorder(this, 1, 1, 0.1, Color.WHITE, Color.BLANK));
-                break;
 
             case 0:
                 root.add(new GUIGradient(this, 1, 1, Color.BLACK));
