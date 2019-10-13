efc58ab6a16bb5eb744727930d2e26c0303f6162
Kyle Koder <silvertallon@gmail.com>
2019-10-12 19:19:48 -0700

    Hardest part is probably done, though it certainly still needs work

--- src/main/java/com/fantasticsource/mctools/gui/element/text/GUIMultilineTextInput.java
+++ src/main/java/com/fantasticsource/mctools/gui/element/text/GUIMultilineTextInput.java
@@ -2,12 +2,15 @@ package com.fantasticsource.mctools.gui.element.text;
 
 import com.fantasticsource.mctools.gui.GUIScreen;
 import com.fantasticsource.mctools.gui.element.text.filter.TextFilter;
+import com.fantasticsource.tools.Tools;
 import com.fantasticsource.tools.datastructures.Color;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.ScaledResolution;
 import net.minecraft.client.renderer.GlStateManager;
+import org.lwjgl.opengl.GL11;
 
 import static com.fantasticsource.mctools.gui.GUIScreen.FONT_RENDERER;
+import static com.fantasticsource.tools.datastructures.Color.GRAY;
 
 public class GUIMultilineTextInput extends GUITextInput
 {
@@ -34,21 +37,110 @@ public class GUIMultilineTextInput extends GUITextInput
     @Override
     public void draw()
     {
-        GlStateManager.enableTexture2D();
+        GlStateManager.disableTexture2D();
 
-        GlStateManager.pushMatrix();
         double scale = new ScaledResolution(Minecraft.getMinecraft()).getScaleFactor();
-        GlStateManager.scale(scale / absolutePxWidth(), scale / absolutePxHeight(), 1);
 
-        Color c = active ? activeColor : isMouseWithin() ? hoverColor : color;
-        int yy = 0;
-        for (String line : lines)
+        if (!filter.acceptable(text))
         {
-            FONT_RENDERER.drawString(line, 0, yy, (c.color() >> 8) | c.a() << 24, false);
-            yy += FONT_RENDERER.FONT_HEIGHT;
+            //Highlight red if text does not pass filter
+            GlStateManager.color(T_RED.rf(), T_RED.gf(), T_RED.bf(), T_RED.af());
+
+            GlStateManager.glBegin(GL11.GL_QUADS);
+            GlStateManager.glVertex3f(0, 0, 0);
+            GlStateManager.glVertex3f(0, 1, 0);
+            GlStateManager.glVertex3f(1, 1, 0);
+            GlStateManager.glVertex3f(1, 0, 0);
+            GlStateManager.glEnd();
+        }
+        else if (active)
+        {
+            //If we pass the filter, highlight gray if active
+            GlStateManager.color(GRAY.rf(), GRAY.gf(), GRAY.bf(), 0.2f);
+
+            GlStateManager.glBegin(GL11.GL_QUADS);
+            GlStateManager.glVertex3f(0, 0, 0);
+            GlStateManager.glVertex3f(0, 1, 0);
+            GlStateManager.glVertex3f(1, 1, 0);
+            GlStateManager.glVertex3f(1, 0, 0);
+            GlStateManager.glEnd();
         }
 
-        GlStateManager.popMatrix();
+
+        //Actual text
+        if (text.length() > 0)
+        {
+            GlStateManager.enableTexture2D();
+
+            GlStateManager.pushMatrix();
+            GlStateManager.scale(scale / absolutePxWidth(), scale / absolutePxHeight(), 1);
+
+            Color c = active ? activeColor : isMouseWithin() ? hoverColor : color;
+            int yy = 0;
+            for (String line : lines)
+            {
+                FONT_RENDERER.drawString(line, 0, yy, (c.color() >> 8) | c.a() << 24, false);
+                yy += FONT_RENDERER.FONT_HEIGHT;
+            }
+
+            GlStateManager.popMatrix();
+
+            GlStateManager.disableTexture2D();
+        }
+
+
+        //Draw cursor and selection highlight
+        if (active)
+        {
+            float lineHeight = 1f / lines.size();
+
+            //Calculate cursor position
+            float cursorX = 0;
+            int lineStart = 0;
+            int cursorLine = 0;
+            for (String fullLine : fullLines)
+            {
+                if (cursorPosition <= lineStart + fullLine.length())
+                {
+                    cursorX = FONT_RENDERER.getStringWidth(fullLine.replace("\n", "").substring(0, cursorPosition - lineStart)) + 0.5f;
+                    break;
+                }
+
+                cursorLine++;
+                lineStart += fullLine.length();
+            }
+
+
+            if (selectorPosition != -1 && selectorPosition != cursorPosition)
+            {
+                //Selection highlight
+//                float selectorX = selectorPosition == -1 ? cursorX : FONT_RENDERER.getStringWidth(text.substring(0, selectorPosition)) - 0.5f;
+//
+//                float min = Tools.min(cursorX, selectorX), max = Tools.max(cursorX, selectorX);
+//                GlStateManager.color(1, 1, 1, 0.3f);
+//
+//                GlStateManager.glBegin(GL11.GL_QUADS);
+//                GlStateManager.glVertex3f(min, 0, 0);
+//                GlStateManager.glVertex3f(min, 1, 0);
+//                GlStateManager.glVertex3f(max, 1, 0);
+//                GlStateManager.glVertex3f(max, 0, 0);
+//                GlStateManager.glEnd();
+            }
+
+            cursorX = Tools.max(cursorX, 1f / absolutePxWidth());
+            cursorX *= scale / absolutePxWidth();
+
+            //Cursor
+            if ((System.currentTimeMillis() - cursorTime) % 1000 < 500)
+            {
+                GlStateManager.color(1, 1, 1, 1);
+
+                GlStateManager.glBegin(GL11.GL_LINES);
+                GlStateManager.glVertex3f(cursorX, cursorLine * lineHeight, 0);
+                GlStateManager.glVertex3f(cursorX, (cursorLine + 1) * lineHeight, 0);
+                GlStateManager.glEnd();
+            }
+        }
 
 
         drawChildren();
