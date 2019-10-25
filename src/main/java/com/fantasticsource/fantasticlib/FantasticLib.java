5d664bd04af975f0cf36095e832d90b0dbf27cc5
Kyle Koder <silvertallon@gmail.com>
2019-10-24 20:20:55 -0700

    1.12.2.025c; fixed item selection GUI not having a faded background

--- src/main/java/com/fantasticsource/fantasticlib/FantasticLib.java
+++ src/main/java/com/fantasticsource/fantasticlib/FantasticLib.java
@@ -21,7 +21,7 @@ public class FantasticLib
 {
     public static final String MODID = "fantasticlib";
     public static final String NAME = "Fantastic Lib";
-    public static final String VERSION = "1.12.2.025b";
+    public static final String VERSION = "1.12.2.025c";
 
 
     private static boolean DEBUG_GUI;
