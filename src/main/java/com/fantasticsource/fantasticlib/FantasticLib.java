37082047d4e425bb1d39c217a494894be52a7709
Kyle Koder <silvertallon@gmail.com>
2019-10-25 19:40:38 -0700

    1.12.2.025d; literal lang key

--- src/main/java/com/fantasticsource/fantasticlib/FantasticLib.java
+++ src/main/java/com/fantasticsource/fantasticlib/FantasticLib.java
@@ -21,7 +21,7 @@ public class FantasticLib
 {
     public static final String MODID = "fantasticlib";
     public static final String NAME = "Fantastic Lib";
-    public static final String VERSION = "1.12.2.025c";
+    public static final String VERSION = "1.12.2.025d";
 
 
     private static boolean DEBUG_GUI;
