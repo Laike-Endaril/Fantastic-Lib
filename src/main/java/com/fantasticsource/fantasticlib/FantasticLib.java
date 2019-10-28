721c61a79bcb77cb987bd15db58bfefa63d62ca3
Kyle Koder <silvertallon@gmail.com>
2019-10-27 18:02:13 -0700

    1.12.2.025e

--- src/main/java/com/fantasticsource/fantasticlib/FantasticLib.java
+++ src/main/java/com/fantasticsource/fantasticlib/FantasticLib.java
@@ -25,7 +25,7 @@ public class FantasticLib
 {
     public static final String MODID = "fantasticlib";
     public static final String NAME = "Fantastic Lib";
-    public static final String VERSION = "1.12.2.025d";
+    public static final String VERSION = "1.12.2.025e";
 
 
     private static boolean DEBUG_GUI;
