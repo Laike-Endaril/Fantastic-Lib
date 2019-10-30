f8ca115097bb82a2fa540a8576dcc4d748da6e2a
Kyle Koder <silvertallon@gmail.com>
2019-10-30 09:58:41 -0700

    Added in-game year, month, day-of-month, hour, minute, second, and season

--- src/main/java/com/fantasticsource/mctools/MCTimestamp.java
+++ src/main/java/com/fantasticsource/mctools/MCTimestamp.java
@@ -29,13 +29,50 @@ public class MCTimestamp extends Timestamp
         this.worldTick = worldTick;
     }
 
-    public int getGameDay()
+
+    public int getGameDayAbsolute()
     {
         return (int) (worldTick / 24000L);
     }
 
-    public int getGameTimeOfDay()
+    public int getGameTimeOfDayAbsolute()
     {
         return (int) (worldTick % 24000L);
+    }
+
+
+    public int getGameYear()
+    {
+        return getGameDayAbsolute() / 360 + 1;
+    }
+
+    public int getGameSeason()
+    {
+        return (getGameDayAbsolute() % 360) / 90 + 1;
+    }
+
+    public int getGameMonth()
+    {
+        return (getGameDayAbsolute() % 360) / 30 + 1;
+    }
+
+    public int getGameDay()
+    {
+        return (getGameDayAbsolute() % 30) + 1;
+    }
+
+    public int getGameHour()
+    {
+        return (int) ((worldTick % 24000L) / 1000L);
+    }
+
+    public int getGameMinute()
+    {
+        return (int) ((worldTick % 1000L) / 16.6666666667);
+    }
+
+    public int getGameSecond()
+    {
+        return (int) (((double) worldTick % 16.6666666667) / 0.27777777777);
     }
 }
