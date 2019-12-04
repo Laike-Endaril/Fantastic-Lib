f3ed5a6a632027a026daff99840b82f74230eccc
Kyle Koder <silvertallon@gmail.com>
2019-12-04 12:57:06 -0800

    Indev build 1.12.2.029a

--- src/main/java/com/fantasticsource/fantasticlib/FantasticLib.java
+++ src/main/java/com/fantasticsource/fantasticlib/FantasticLib.java
@@ -25,7 +25,7 @@ public class FantasticLib
 {
     public static final String MODID = "fantasticlib";
     public static final String NAME = "Fantastic Lib";
-    public static final String VERSION = "1.12.2.029";
+    public static final String VERSION = "1.12.2.029a";
 
 
     public static boolean isClient = false;
