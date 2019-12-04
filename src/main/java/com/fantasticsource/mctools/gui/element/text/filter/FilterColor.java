45fca6a39e18c401f39161a3fe6658a877c9ef55
Kyle Koder <silvertallon@gmail.com>
2019-12-03 17:35:03 -0800

    FilterColor

---
+++ src/main/java/com/fantasticsource/mctools/gui/element/text/filter/FilterColor.java
@@ -0,0 +1,39 @@
+package com.fantasticsource.mctools.gui.element.text.filter;
+
+public class FilterColor extends TextFilter<Integer>
+{
+    public static final FilterColor INSTANCE = new FilterColor();
+
+    private FilterColor()
+    {
+    }
+
+    @Override
+    public String transformInput(String input)
+    {
+        return input.trim().replace("0x", "");
+    }
+
+    @Override
+    public boolean acceptable(String input)
+    {
+        String transformed = transformInput(input);
+        if (input.equals("") || input.length() > 8) return false;
+
+        for (char c : transformed.toCharArray())
+        {
+            if (c >= '0' && c <= '9') continue;
+            if (c >= 'a' && c <= 'f') continue;
+            if (c >= 'A' && c <= 'F') continue;
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public Integer parse(String input)
+    {
+        return !acceptable(input) ? null : Integer.parseInt(input);
+    }
+}
