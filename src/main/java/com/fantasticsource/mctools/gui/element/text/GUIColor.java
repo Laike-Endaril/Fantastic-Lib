0af5b5c2839c3601dc4d3f095e2b27ed4ee8aefe
Kyle Koder <silvertallon@gmail.com>
2019-12-03 16:57:42 -0800

    GUIColor

---
+++ src/main/java/com/fantasticsource/mctools/gui/element/text/GUIColor.java
@@ -0,0 +1,56 @@
+package com.fantasticsource.mctools.gui.element.text;
+
+import com.fantasticsource.mctools.gui.GUIScreen;
+import com.fantasticsource.tools.datastructures.Color;
+
+public class GUIColor extends GUIText
+{
+    private Color value;
+
+    public GUIColor(GUIScreen screen, Color value)
+    {
+        this(screen, value, 1);
+    }
+
+    public GUIColor(GUIScreen screen, Color value, double scale)
+    {
+        super(screen, value == null ? "00000000" : value.hex8(), scale);
+        this.value = value == null ? new Color(0) : value;
+
+        color = GUIScreen.getIdleColor(this.value);
+        hoverColor = GUIScreen.getHoverColor(this.value);
+        activeColor = this.value;
+    }
+
+
+    public GUIColor(GUIScreen screen, double x, double y, Color value)
+    {
+        this(screen, x, y, value, 1);
+    }
+
+    public GUIColor(GUIScreen screen, double x, double y, Color value, double scale)
+    {
+        super(screen, x, y, value == null ? "00000000" : value.hex8(), scale);
+        this.value = value == null ? new Color(0) : value;
+
+        color = GUIScreen.getIdleColor(this.value);
+        hoverColor = GUIScreen.getHoverColor(this.value);
+        activeColor = this.value;
+    }
+
+    public Color getValue()
+    {
+        return value;
+    }
+
+    public GUIColor setValue(Color value)
+    {
+        this.value = value == null ? new Color(0) : value;
+
+        color = GUIScreen.getIdleColor(this.value);
+        hoverColor = GUIScreen.getHoverColor(this.value);
+        activeColor = this.value;
+
+        return this;
+    }
+}
