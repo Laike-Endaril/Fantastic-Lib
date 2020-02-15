0876be485162c7b69e77b35543b43353b859c442
Kyle Koder <silvertallon@gmail.com>
2020-02-14 18:13:25 -0800

    1.12.2.033b; "None" slotting

--- src/main/java/com/fantasticsource/fantasticlib/FantasticLib.java
+++ src/main/java/com/fantasticsource/fantasticlib/FantasticLib.java
@@ -25,7 +25,7 @@ public class FantasticLib
 {
     public static final String MODID = "fantasticlib";
     public static final String NAME = "Fantastic Lib";
-    public static final String VERSION = "1.12.2.033a";
+    public static final String VERSION = "1.12.2.033b";
 
 
     public static boolean isClient = false;
