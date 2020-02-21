076daab2a69a8abc5bdb45eb9b45e052c3dbf185
Kyle Koder <silvertallon@gmail.com>
2020-02-21 03:58:00 -0800

    Compat improvements to new event

--- src/main/java/com/fantasticsource/mctools/event/LivingBaseInventoryChangedEvent.java
+++ src/main/java/com/fantasticsource/mctools/event/LivingBaseInventoryChangedEvent.java
@@ -1,10 +1,15 @@
 package com.fantasticsource.mctools.event;
 
+import baubles.api.BaublesApi;
+import baubles.api.cap.IBaublesItemHandler;
+import com.fantasticsource.fantasticlib.Compat;
 import com.fantasticsource.mctools.items.ItemMatcher;
+import com.fantasticsource.tools.ReflectionTool;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.living.LivingEvent;
@@ -13,11 +18,30 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 import net.minecraftforge.fml.relauncher.Side;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.UUID;
 
 public class LivingBaseInventoryChangedEvent extends LivingEvent
 {
+    public static Class tiamatPlayerInventoryClass = Compat.tiamatrpg ? ReflectionTool.getClassByName("com.fantasticsource.tiamatrpg.inventory.TiamatPlayerInventory") : null;
+    public static Field tiamatServerInventoriesField = tiamatPlayerInventoryClass == null ? null : ReflectionTool.getField(tiamatPlayerInventoryClass, "tiamatServerInventories");
+    public static LinkedHashMap<UUID, IInventory> tiamatServerInventories = null;
+
+    static
+    {
+        try
+        {
+            tiamatServerInventories = (LinkedHashMap<UUID, IInventory>) tiamatServerInventoriesField.get(null);
+        }
+        catch (IllegalAccessException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+
     public static LinkedHashMap<Entity, ArrayList<ItemStack>> previousContents = new LinkedHashMap<>();
 
     static
@@ -61,10 +85,29 @@ public class LivingBaseInventoryChangedEvent extends LivingEvent
             newInventory = new ArrayList<>();
             if (livingBase instanceof EntityPlayerMP)
             {
-                InventoryPlayer inventory = ((EntityPlayerMP) livingBase).inventory;
+                EntityPlayerMP player = (EntityPlayerMP) livingBase;
+                InventoryPlayer inventory = player.inventory;
                 for (int i = 0; i < inventory.getSizeInventory(); i++)
                 {
                     newInventory.add(inventory.getStackInSlot(i));
+                }
+
+                if (Compat.baubles)
+                {
+                    IBaublesItemHandler baublesInventory = BaublesApi.getBaublesHandler(player);
+                    for (int i = 0; i < baublesInventory.getSlots(); i++)
+                    {
+                        newInventory.add(baublesInventory.getStackInSlot(i));
+                    }
+                }
+
+                if (tiamatServerInventories != null)
+                {
+                    IInventory tiamatInventory = tiamatServerInventories.get(player);
+                    for (int i = 0; i < tiamatInventory.getSizeInventory(); i++)
+                    {
+                        newInventory.add(tiamatInventory.getStackInSlot(i));
+                    }
                 }
             }
             else
