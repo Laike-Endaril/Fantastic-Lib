91e6b6cf9e91cc93a7f8434dc8a3f74a2b66c2ee
Kyle Koder <silvertallon@gmail.com>
2020-02-23 10:13:11 -0800

    Benchmark improvements

--- src/main/java/com/fantasticsource/tools/Benchmark.java
+++ src/main/java/com/fantasticsource/tools/Benchmark.java
@@ -1,20 +1,35 @@
 package com.fantasticsource.tools;
 
 import javax.annotation.Nullable;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 
 public class Benchmark
 {
     public static void benchmark(int iterations, @Nullable String[] labels, Runnable... runnables)
     {
-        long time;
+        long iTime, totalTime = 0;
         int i, i2 = 0;
+        long[] times = new long[runnables.length];
         for (Runnable runnable : runnables)
         {
             i = 0;
-            time = System.nanoTime();
+            iTime = System.nanoTime();
             for (; i < iterations; i++) runnable.run();
-            time = System.nanoTime() - time;
-            System.out.println(((labels != null && i2 < labels.length) ? labels[i2++] + ": " : "") + time);
+            iTime = System.nanoTime() - iTime;
+            times[i2++] = iTime;
+            totalTime += iTime;
+        }
+
+        NumberFormat timeFormat = NumberFormat.getInstance(), percentFormat = DecimalFormat.getInstance();
+        String label, timeString, percent;
+        i2 = 0;
+        for (long time : times)
+        {
+            label = labels != null && i2 < labels.length ? labels[i2++] + ": " : "";
+            timeString = timeFormat.format(time) + " nanos";
+            percent = percentFormat.format((double) (time / totalTime) * 100);
+            System.out.println(label + timeString + " (" + percent + "%)");
         }
     }
 }
