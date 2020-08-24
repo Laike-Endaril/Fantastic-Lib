57d6c695f5ab89d0abde8d17bd5afc7cd5b06372
Kyle Koder <silvertallon@gmail.com>
2020-08-24 10:51:36 -0700

    Added to GlobalInventory.getItem()

--- src/main/java/com/fantasticsource/mctools/GlobalInventory.java
+++ src/main/java/com/fantasticsource/mctools/GlobalInventory.java
@@ -958,6 +958,42 @@ public class GlobalInventory
             case "feet":
                 return getVanillaFootItem(entity);
 
+            case "hotbar1":
+                if (entity instanceof EntityPlayerMP) return ((EntityPlayerMP) entity).inventory.getStackInSlot(0);
+                return null;
+
+            case "hotbar2":
+                if (entity instanceof EntityPlayerMP) return ((EntityPlayerMP) entity).inventory.getStackInSlot(1);
+                return null;
+
+            case "hotbar3":
+                if (entity instanceof EntityPlayerMP) return ((EntityPlayerMP) entity).inventory.getStackInSlot(2);
+                return null;
+
+            case "hotbar4":
+                if (entity instanceof EntityPlayerMP) return ((EntityPlayerMP) entity).inventory.getStackInSlot(3);
+                return null;
+
+            case "hotbar5":
+                if (entity instanceof EntityPlayerMP) return ((EntityPlayerMP) entity).inventory.getStackInSlot(4);
+                return null;
+
+            case "hotbar6":
+                if (entity instanceof EntityPlayerMP) return ((EntityPlayerMP) entity).inventory.getStackInSlot(5);
+                return null;
+
+            case "hotbar7":
+                if (entity instanceof EntityPlayerMP) return ((EntityPlayerMP) entity).inventory.getStackInSlot(6);
+                return null;
+
+            case "hotbar8":
+                if (entity instanceof EntityPlayerMP) return ((EntityPlayerMP) entity).inventory.getStackInSlot(7);
+                return null;
+
+            case "hotbar9":
+                if (entity instanceof EntityPlayerMP) return ((EntityPlayerMP) entity).inventory.getStackInSlot(8);
+                return null;
+
             //Tiamat
             case "mainhand1":
                 return getTiamatSheathedMainhand1(entity);
