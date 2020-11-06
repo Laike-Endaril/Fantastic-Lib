3f2d6f23dbeb30366173b00f1766b5bd42723c9d
Kyle Koder <silvertallon@gmail.com>
2020-11-06 10:33:48 -0800

    1.12.2.039d

--- src/main/java/com/fantasticsource/fantasticlib/FantasticLib.java
+++ src/main/java/com/fantasticsource/fantasticlib/FantasticLib.java
@@ -33,7 +33,7 @@ public class FantasticLib
     public static final String MODID = "fantasticlib";
     public static final String DOMAIN = "flib";
     public static final String NAME = "Fantastic Lib";
-    public static final String VERSION = "1.12.2.039c";
+    public static final String VERSION = "1.12.2.039d";
 
 
     public static boolean isClient = false;
