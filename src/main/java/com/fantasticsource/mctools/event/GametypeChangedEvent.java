8143f8e3b3dd784b31d9735c55735123fd0697f7
Kyle Koder <silvertallon@gmail.com>
2020-12-14 13:38:42 -0800

    GametypeChangeEvent improvements

--- src/main/java/com/fantasticsource/mctools/event/GametypeChangedEvent.java
+++ src/main/java/com/fantasticsource/mctools/event/GametypeChangedEvent.java
@@ -1,7 +1,9 @@
 package com.fantasticsource.mctools.event;
 
 import com.fantasticsource.mctools.MCTools;
+import com.fantasticsource.tools.datastructures.Pair;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.world.GameType;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.event.FMLServerStoppedEvent;
@@ -15,7 +17,7 @@ import java.util.UUID;
 
 public class GametypeChangedEvent extends Event
 {
-    public static final HashMap<UUID, GameType> PLAYER_GAMETYPES = new HashMap<>();
+    public static final HashMap<Pair<UUID, Boolean>, GameType> PLAYER_GAMETYPES = new HashMap<>();
 
     static
     {
@@ -35,7 +37,7 @@ public class GametypeChangedEvent extends Event
     @SubscribeEvent
     public static void playerLogoff(PlayerEvent.PlayerLoggedOutEvent event)
     {
-        GameType oldGameType = PLAYER_GAMETYPES.remove(event.player.getPersistentID());
+        GameType oldGameType = PLAYER_GAMETYPES.remove(new Pair<>(event.player.getPersistentID(), event.player instanceof EntityPlayerMP));
         if (oldGameType != null) MinecraftForge.EVENT_BUS.post(new GametypeChangedEvent(event.player, oldGameType, null));
     }
 
@@ -50,10 +52,11 @@ public class GametypeChangedEvent extends Event
         if (event.phase == TickEvent.Phase.END) return;
 
         EntityPlayer player = event.player;
-        GameType oldGameType = PLAYER_GAMETYPES.get(player.getPersistentID()), newGameType = MCTools.getGameType(player);
+        Pair<UUID, Boolean> pair = new Pair<>(player.getPersistentID(), player instanceof EntityPlayerMP);
+        GameType oldGameType = PLAYER_GAMETYPES.get(pair), newGameType = MCTools.getGameType(player);
         if (oldGameType != newGameType)
         {
-            PLAYER_GAMETYPES.put(player.getPersistentID(), newGameType);
+            PLAYER_GAMETYPES.put(pair, newGameType);
             MinecraftForge.EVENT_BUS.post(new GametypeChangedEvent(player, oldGameType, newGameType));
         }
     }
