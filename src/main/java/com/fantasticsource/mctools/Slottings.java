4a4af3dbbf5e2b7d89b6a7cc1ee3cf5377c29270
Kyle Koder <silvertallon@gmail.com>
2020-12-31 15:31:33 -0800

    Slottings.slottingIsValidForSlot()

--- src/main/java/com/fantasticsource/mctools/Slottings.java
+++ src/main/java/com/fantasticsource/mctools/Slottings.java
@@ -1,82 +1,94 @@
 package com.fantasticsource.mctools;
 
 import com.fantasticsource.fantasticlib.Compat;
+import com.fantasticsource.tools.Tools;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 
-import java.util.ArrayList;
+import java.util.LinkedHashMap;
 
 public class Slottings
 {
     protected static final String DOMAIN = "tiamatrpg";
 
-    public static final ArrayList<String> SLOTS_VANILLA = new ArrayList<>();
-    public static final ArrayList<String> SLOTS_BAUBLES = new ArrayList<>();
-    public static final ArrayList<String> SLOTS_TIAMAT_INVENTORY = new ArrayList<>();
+    public static final LinkedHashMap<String, String[]> SLOTS_VANILLA = new LinkedHashMap<>();
+    public static final LinkedHashMap<String, String[]> SLOTS_BAUBLES = new LinkedHashMap<>();
+    public static final LinkedHashMap<String, String[]> SLOTS_TIAMAT_INVENTORY = new LinkedHashMap<>();
+
+    public static final LinkedHashMap<String, String[]> SLOTS_ALL = new LinkedHashMap<>();
+    public static final LinkedHashMap<String, String[]> SLOTS_AVAILABLE = new LinkedHashMap<>();
 
     static
     {
-        SLOTS_VANILLA.add("None");
+        //Itemstack slotting -> valid slot types
+        SLOTS_VANILLA.put("None", new String[]{});
 
-        SLOTS_VANILLA.add("Mainhand");
-        SLOTS_VANILLA.add("Offhand");
-        SLOTS_VANILLA.add("Hand");
+        SLOTS_VANILLA.put("Mainhand", new String[]{"Mainhand"});
+        SLOTS_VANILLA.put("Offhand", new String[]{"Offhand"});
+        SLOTS_VANILLA.put("Hand", new String[]{"Mainhand", "Offhand"});
 
-        SLOTS_VANILLA.add("Head");
-        SLOTS_VANILLA.add("Chest");
-        SLOTS_VANILLA.add("Legs");
-        SLOTS_VANILLA.add("Feet");
-        SLOTS_VANILLA.add("Armor");
+        SLOTS_VANILLA.put("Head", new String[]{"Head"});
+        SLOTS_VANILLA.put("Chest", new String[]{"Chest"});
+        SLOTS_VANILLA.put("Legs", new String[]{"Legs"});
+        SLOTS_VANILLA.put("Feet", new String[]{"Feet"});
+        SLOTS_VANILLA.put("Armor", new String[]{"Head", "Chest", "Legs", "Feet"});
 
-        SLOTS_VANILLA.add("Hotbar");
-        SLOTS_VANILLA.add("Cargo");
-        SLOTS_VANILLA.add("Any");
-
-
-        SLOTS_BAUBLES.add("Baubles Amulet");
-        SLOTS_BAUBLES.add("Baubles Ring");
-        SLOTS_BAUBLES.add("Baubles Belt");
-        SLOTS_BAUBLES.add("Baubles Head");
-        SLOTS_BAUBLES.add("Baubles Body");
-        SLOTS_BAUBLES.add("Baubles Charm");
-        SLOTS_BAUBLES.add("Baubles Trinket");
+        SLOTS_VANILLA.put("Hotbar", new String[]{"Hotbar", "Mainhand"});
+        SLOTS_VANILLA.put("Cargo", new String[]{"Cargo"});
+        SLOTS_VANILLA.put("Any", new String[]{"Mainhand", "Offhand", "Head", "Chest", "Legs", "Feet", "Hotbar", "Cargo", "Baubles Amulet", "Baubles Ring", "Baubles Belt", "Baubles Head", "Baubles Body", "Baubles Charm", "Tiamat Shoulders", "Tiamat Cape", "Tiamat Quickslot", "Tiamat Backpack", "Tiamat Pet", "Tiamat Deck", "Tiamat Class", "Tiamat Offensive Skill", "Tiamat Utility Skill", "Tiamat Ultimate Skill", "Tiamat Passive Skill", "Tiamat Gathering Profession", "Tiamat Crafting Profession", "Tiamat Recipe"});
 
 
-        SLOTS_TIAMAT_INVENTORY.add("Tiamat 2H");
+        SLOTS_BAUBLES.put("Baubles Amulet", new String[]{"Baubles Amulet"});
+        SLOTS_BAUBLES.put("Baubles Ring", new String[]{"Baubles Ring"});
+        SLOTS_BAUBLES.put("Baubles Belt", new String[]{"Baubles Belt"});
+        SLOTS_BAUBLES.put("Baubles Head", new String[]{"Baubles Head"});
+        SLOTS_BAUBLES.put("Baubles Body", new String[]{"Baubles Body"});
+        SLOTS_BAUBLES.put("Baubles Charm", new String[]{"Baubles Charm"});
+        SLOTS_BAUBLES.put("Baubles Trinket", new String[]{"Baubles Amulet", "Baubles Ring", "Baubles Belt", "Baubles Head", "Baubles Body", "Baubles Charm"});
 
-        SLOTS_TIAMAT_INVENTORY.add("Tiamat Shoulders");
-        SLOTS_TIAMAT_INVENTORY.add("Tiamat Cape");
 
-        SLOTS_TIAMAT_INVENTORY.add("Tiamat Quickslot");
+        SLOTS_TIAMAT_INVENTORY.put("Tiamat 2H", new String[]{"Mainhand", "Offhand"});
 
-        SLOTS_TIAMAT_INVENTORY.add("Tiamat Backpack");
+        SLOTS_TIAMAT_INVENTORY.put("Tiamat Shoulders", new String[]{"Tiamat Shoulders"});
+        SLOTS_TIAMAT_INVENTORY.put("Tiamat Cape", new String[]{"Tiamat Cape"});
 
-        SLOTS_TIAMAT_INVENTORY.add("Tiamat Pet");
+        SLOTS_TIAMAT_INVENTORY.put("Tiamat Quickslot", new String[]{"Tiamat Quickslot"});
 
-        SLOTS_TIAMAT_INVENTORY.add("Tiamat Deck");
+        SLOTS_TIAMAT_INVENTORY.put("Tiamat Backpack", new String[]{"Tiamat Backpack"});
 
-        SLOTS_TIAMAT_INVENTORY.add("Tiamat Class");
+        SLOTS_TIAMAT_INVENTORY.put("Tiamat Pet", new String[]{"Tiamat Pet"});
 
-        SLOTS_TIAMAT_INVENTORY.add("Tiamat Offensive Skill");
-        SLOTS_TIAMAT_INVENTORY.add("Tiamat Utility Skill");
-        SLOTS_TIAMAT_INVENTORY.add("Tiamat Ultimate Skill");
-        SLOTS_TIAMAT_INVENTORY.add("Tiamat Passive Skill");
-        SLOTS_TIAMAT_INVENTORY.add("Tiamat Skill");
+        SLOTS_TIAMAT_INVENTORY.put("Tiamat Deck", new String[]{"Tiamat Deck"});
 
-        SLOTS_TIAMAT_INVENTORY.add("Tiamat Gathering Profession");
-        SLOTS_TIAMAT_INVENTORY.add("Tiamat Crafting Profession");
-        SLOTS_TIAMAT_INVENTORY.add("Tiamat Profession");
+        SLOTS_TIAMAT_INVENTORY.put("Tiamat Class", new String[]{"Tiamat Class"});
 
-        SLOTS_TIAMAT_INVENTORY.add("Tiamat Recipe");
+        SLOTS_TIAMAT_INVENTORY.put("Tiamat Offensive Skill", new String[]{"Tiamat Offensive Skill"});
+        SLOTS_TIAMAT_INVENTORY.put("Tiamat Utility Skill", new String[]{"Tiamat Utility Skill"});
+        SLOTS_TIAMAT_INVENTORY.put("Tiamat Ultimate Skill", new String[]{"Tiamat Ultimate Skill"});
+        SLOTS_TIAMAT_INVENTORY.put("Tiamat Passive Skill", new String[]{"Tiamat Passive Skill"});
+        SLOTS_TIAMAT_INVENTORY.put("Tiamat Skill", new String[]{"Tiamat Offensive Skill", "Tiamat Utility Skill", "Tiamat Ultimate Skill", "Tiamat Passive Skill"});
+
+        SLOTS_TIAMAT_INVENTORY.put("Tiamat Gathering Profession", new String[]{"Tiamat Gathering Profession"});
+        SLOTS_TIAMAT_INVENTORY.put("Tiamat Crafting Profession", new String[]{"Tiamat Crafting Profession"});
+        SLOTS_TIAMAT_INVENTORY.put("Tiamat Profession", new String[]{"Tiamat Gathering Profession", "Tiamat Crafting Profession"});
+
+        SLOTS_TIAMAT_INVENTORY.put("Tiamat Recipe", new String[]{"Tiamat Recipe"});
+
+
+        SLOTS_ALL.putAll(SLOTS_VANILLA);
+        SLOTS_ALL.putAll(SLOTS_BAUBLES);
+        SLOTS_ALL.putAll(SLOTS_TIAMAT_INVENTORY);
+
+
+        SLOTS_AVAILABLE.putAll(SLOTS_VANILLA);
+        if (Compat.baubles) SLOTS_AVAILABLE.putAll(SLOTS_BAUBLES);
+        if (Compat.tiamatinventory) SLOTS_AVAILABLE.putAll(SLOTS_TIAMAT_INVENTORY);
     }
 
-    public static ArrayList<String> availableSlottings()
+    public static String[] availableSlottings()
     {
-        ArrayList<String> result = new ArrayList<>(SLOTS_VANILLA);
-        if (Compat.baubles) result.addAll(SLOTS_BAUBLES);
-        if (Compat.tiamatinventory) result.addAll(SLOTS_TIAMAT_INVENTORY);
-        return result;
+        return SLOTS_AVAILABLE.keySet().toArray(new String[0]);
     }
 
 
@@ -128,8 +140,15 @@ public class Slottings
         }
     }
 
+
     public static boolean isTwoHanded(ItemStack stack)
     {
         return stack.getItem() == Items.BOW || getItemSlotting(stack).equals("Tiamat 2H");
+    }
+
+
+    public static boolean slottingIsValidForSlot(String itemSlotting, String slotType)
+    {
+        return SLOTS_AVAILABLE.containsKey(itemSlotting) && Tools.contains(SLOTS_AVAILABLE.get(itemSlotting), slotType);
     }
 }
