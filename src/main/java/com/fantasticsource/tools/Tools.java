25c6d134faf7f9bf142b7079ed99989fdad35b58
Kyle Koder <silvertallon@gmail.com>
2021-01-04 14:47:49 -0800

    Tools.calc

--- src/main/java/com/fantasticsource/tools/Tools.java
+++ src/main/java/com/fantasticsource/tools/Tools.java
@@ -19,6 +19,119 @@ public class Tools
     protected static PrintStream out = null, err = null;
 
 
+    //Thanks to Boann at https://stackoverflow.com/questions/3422673/how-to-evaluate-a-math-expression-given-in-string-form
+    //For the original source
+    public static double calc(final String str)
+    {
+        return new Object()
+        {
+            int pos = -1, ch;
+
+            void nextChar()
+            {
+                ch = (++pos < str.length()) ? str.charAt(pos) : -1;
+            }
+
+            boolean eat(int charToEat)
+            {
+                while (ch == ' ') nextChar();
+                if (ch == charToEat)
+                {
+                    nextChar();
+                    return true;
+                }
+                return false;
+            }
+
+            double parse()
+            {
+                nextChar();
+                double x = parseExpression();
+                if (pos < str.length()) throw new RuntimeException("Unexpected: " + (char) ch);
+                return x;
+            }
+
+            // Grammar:
+            // expression = term | expression `+` term | expression `-` term
+            // term = factor | term `*` factor | term `/` factor
+            // factor = `+` factor | `-` factor | `(` expression `)`
+            //        | number | functionName factor | factor `^` factor
+
+            double parseExpression()
+            {
+                double x = parseTerm();
+                for (; ; )
+                {
+                    if (eat('+')) x += parseTerm(); // addition
+                    else if (eat('-')) x -= parseTerm(); // subtraction
+                    else return x;
+                }
+            }
+
+            double parseTerm()
+            {
+                double x = parseFactor();
+                for (; ; )
+                {
+                    if (eat('*')) x *= parseFactor(); // multiplication
+                    else if (eat('/')) x /= parseFactor(); // division
+                    else return x;
+                }
+            }
+
+            double parseFactor()
+            {
+                if (eat('+')) return parseFactor(); // unary plus
+                if (eat('-')) return -parseFactor(); // unary minus
+
+                double x;
+                int startPos = this.pos;
+                if (eat('('))
+                { // parentheses
+                    x = parseExpression();
+                    eat(')');
+                }
+                else if ((ch >= '0' && ch <= '9') || ch == '.')
+                { // numbers
+                    while ((ch >= '0' && ch <= '9') || ch == '.') nextChar();
+                    x = Double.parseDouble(str.substring(startPos, this.pos));
+                }
+                else if (ch >= 'a' && ch <= 'z')
+                { // functions
+                    while (ch >= 'a' && ch <= 'z') nextChar();
+                    String func = str.substring(startPos, this.pos);
+                    x = parseFactor();
+                    switch (func)
+                    {
+                        case "sqrt":
+                            x = Math.sqrt(x);
+                            break;
+                        case "sin":
+                            x = TrigLookupTable.TRIG_TABLE_1024.sin(degtorad(x));
+                            break;
+                        case "cos":
+                            x = TrigLookupTable.TRIG_TABLE_1024.cos(degtorad(x));
+                            break;
+                        case "tan":
+                            x = TrigLookupTable.TRIG_TABLE_1024.tan(degtorad(x));
+                            break;
+                        default:
+                            throw new RuntimeException("Unknown function: " + func);
+                    }
+                }
+                else
+                {
+                    throw new RuntimeException("Unexpected: " + (char) ch);
+                }
+
+                if (eat('^')) x = Math.pow(x, parseFactor()); // exponentiation
+
+                return x;
+            }
+        }.parse();
+    }
+
+
     public static BufferedReader getJarResourceReader(Class classInJar, String resourcePathAndName)
     {
         return new BufferedReader(new InputStreamReader(getJarResourceStream(classInJar, resourcePathAndName)));
