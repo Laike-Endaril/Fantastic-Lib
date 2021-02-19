f987468725ed48289f4d31a1be8e7ad648124574
Kyle Koder <silvertallon@gmail.com>
2021-02-19 11:23:35 -0800

    1.12.2.044zf

--- src/main/java/com/fantasticsource/fantasticlib/FantasticLib.java
+++ src/main/java/com/fantasticsource/fantasticlib/FantasticLib.java
@@ -44,7 +44,7 @@ public class FantasticLib
     public static final String MODID = "fantasticlib";
     public static final String DOMAIN = "flib";
     public static final String NAME = "Fantastic Lib";
-    public static final String VERSION = "1.12.2.044ze";
+    public static final String VERSION = "1.12.2.044zf";
 
 
     public static long serverStartTime = -1;
