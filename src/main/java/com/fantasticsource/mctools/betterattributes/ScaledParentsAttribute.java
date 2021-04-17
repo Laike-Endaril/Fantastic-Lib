25d6298338c22a2ada5d974047d8432d77b17215
Kyle Koder <silvertallon@gmail.com>
2021-04-16 22:06:50 -0700

    1.12.2.044zzm; one more constructor for ScaledParentsAttribute

--- src/main/java/com/fantasticsource/mctools/betterattributes/ScaledParentsAttribute.java
+++ src/main/java/com/fantasticsource/mctools/betterattributes/ScaledParentsAttribute.java
@@ -1,5 +1,6 @@
 package com.fantasticsource.mctools.betterattributes;
 
+import com.fantasticsource.tools.datastructures.Pair;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.text.TextFormatting;
 
@@ -11,6 +12,11 @@ public class ScaledParentsAttribute extends BetterAttribute
     public final HashMap<BetterAttribute, Double> parentMultipliers;
 
     public ScaledParentsAttribute(String name, boolean isGood, double defaultBaseAmount, BetterAttribute... parents)
+    {
+        this(name, isGood, defaultBaseAmount, generateDefaults(parents));
+    }
+
+    public ScaledParentsAttribute(String name, boolean isGood, double defaultBaseAmount, Pair<BetterAttribute, Double>... parents)
     {
         this(name, isGood, defaultBaseAmount, generateDefaults(parents));
     }
@@ -45,6 +51,13 @@ public class ScaledParentsAttribute extends BetterAttribute
     {
         HashMap<BetterAttribute, Double> result = new HashMap<>();
         for (BetterAttribute parent : parents) result.put(parent, 1d / parents.length);
+        return result;
+    }
+
+    protected static HashMap<BetterAttribute, Double> generateDefaults(Pair<BetterAttribute, Double>... parents)
+    {
+        HashMap<BetterAttribute, Double> result = new HashMap<>();
+        for (Pair<BetterAttribute, Double> pair : parents) result.put(pair.getKey(), pair.getValue());
         return result;
     }
 
