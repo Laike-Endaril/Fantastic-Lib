e2befd683ce515ba13c0104fd689c0024e4c3b06
Kyle Koder <silvertallon@gmail.com>
2021-04-17 02:42:39 -0700

    Expanded MultipliedParentsAttribute

--- src/main/java/com/fantasticsource/mctools/betterattributes/MultipliedParentsAttribute.java
+++ src/main/java/com/fantasticsource/mctools/betterattributes/MultipliedParentsAttribute.java
@@ -1,19 +1,33 @@
 package com.fantasticsource.mctools.betterattributes;
 
+import com.fantasticsource.tools.datastructures.Pair;
 import net.minecraft.entity.Entity;
 
-public class MultipliedParentsAttribute extends BetterAttribute
+import java.util.HashMap;
+import java.util.Map;
+
+public class MultipliedParentsAttribute extends ScaledParentsAttribute
 {
     public MultipliedParentsAttribute(String name, boolean isGood, double defaultBaseAmount, BetterAttribute... parents)
     {
-        super(name, isGood, defaultBaseAmount, true, parents);
+        super(name, isGood, defaultBaseAmount, parents);
+    }
+
+    public MultipliedParentsAttribute(String name, boolean isGood, double defaultBaseAmount, Pair<BetterAttribute, Double>... parents)
+    {
+        super(name, isGood, defaultBaseAmount, parents);
+    }
+
+    public MultipliedParentsAttribute(String name, boolean isGood, double defaultBaseAmount, HashMap<BetterAttribute, Double> parents)
+    {
+        super(name, isGood, defaultBaseAmount, parents);
     }
 
     @Override
     public double calculateTotalAmount(Entity entity)
     {
         double result = getBaseAmount(entity);
-        for (BetterAttribute parent : parents) result *= parent.getTotalAmount(entity);
+        for (Map.Entry<BetterAttribute, Double> entry : parentMultipliers.entrySet()) result *= entry.getKey().getTotalAmount(entity) * entry.getValue();
         return result;
     }
 }
