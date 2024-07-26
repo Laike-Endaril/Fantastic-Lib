90dcf2ab4cb2c4d667acab15b48a06820b6dac80
Kyle Koder <silvertallon@gmail.com>
2024-07-26 14:10:24 -0700

    Better mod compat for tooltip scaling

--- src/main/java/com/fantasticsource/mctools/TooltipAlterer.java
+++ src/main/java/com/fantasticsource/mctools/TooltipAlterer.java
@@ -3,11 +3,12 @@ package com.fantasticsource.mctools;
 import com.fantasticsource.fantasticlib.config.FantasticConfig;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraftforge.client.event.RenderTooltipEvent;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class TooltipAlterer
 {
-    @SubscribeEvent
+    @SubscribeEvent(priority = EventPriority.LOWEST)
     public static void renderTooltipPre(RenderTooltipEvent.Pre event)
     {
         double scale = FantasticConfig.tooltipScaling;
@@ -16,7 +17,7 @@ public class TooltipAlterer
         GlStateManager.scale(scale, scale, scale);
     }
 
-    @SubscribeEvent
+    @SubscribeEvent(priority = EventPriority.HIGHEST)
     public static void renderTooltipPost(RenderTooltipEvent.PostText event)
     {
         GlStateManager.popMatrix();
