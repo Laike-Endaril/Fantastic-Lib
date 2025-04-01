604656ec9381d2378a8b51a6b10751d14b4bdb27
Laike-Endaril <silvertallon@gmail.com>
2025-04-01 12:05:28 -0700

    Part of a fix for vanilla's incorrect potion combining, used in Set Bonus

--- src/main/java/com/fantasticsource/mctools/potions/FantasticPotionEffect.java
+++ src/main/java/com/fantasticsource/mctools/potions/FantasticPotionEffect.java
@@ -1,10 +1,16 @@
 package com.fantasticsource.mctools.potions;
 
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
+import net.minecraftforge.fml.common.FMLCommonHandler;
+import net.minecraftforge.fml.relauncher.Side;
 
 public class FantasticPotionEffect extends PotionEffect
 {
+    //Found in SPacketEntityEffect, when transferring potion effects from server to client
+    public static final int MAX_DURATION_THRESHOLD = 32767;
+
     public int interval = 0;
 
     public FantasticPotionEffect(Potion potionIn)
@@ -36,6 +42,32 @@ public class FantasticPotionEffect extends PotionEffect
     {
         this.interval = interval;
         return this;
+    }
+
+
+    @Override
+    public void combine(PotionEffect other)
+    {
+        //isPotionDurationMax, setPotionDurationMax(), and getPotionDurationMax() are client-side-only
+        if (FMLCommonHandler.instance().getEffectiveSide() == Side.CLIENT && getIsPotionDurationMax() && (other.getAmplifier() > getAmplifier()))
+        {
+            setPotionDurationMax(false);
+            super.combine(other);
+            if (getDuration() >= MAX_DURATION_THRESHOLD) setPotionDurationMax(true);
+        }
+        else super.combine(other);
+    }
+
+    @Override
+    public boolean onUpdate(EntityLivingBase entityIn)
+    {
+        if (getDuration() >= 32767)
+        {
+            //Skip decrementation if we're supposed to be infinite
+            if (getPotion().isReady(getDuration(), getAmplifier())) performEffect(entityIn);
+            return true;
+        }
+        else return super.onUpdate(entityIn);
     }
 
     @Override
