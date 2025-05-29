7f80699114bd18a9e9c3f94df0aa917faafd952e
Laike-Endaril <silvertallon@gmail.com>
2025-05-28 20:18:45 -0700

    1.12.2.067

--- src/main/java/com/fantasticsource/fantasticlib/FantasticLib.java
+++ src/main/java/com/fantasticsource/fantasticlib/FantasticLib.java
@@ -34,7 +34,7 @@ public class FantasticLib
     public static final String MODID = "fantasticlib";
     public static final String DOMAIN = "flib"; //Referenced in some other mods
     public static final String NAME = "Fantastic Lib";
-    public static final String VERSION = "1.12.2.066";
+    public static final String VERSION = "1.12.2.067";
 
 
     public static long serverStartTime = -1;
